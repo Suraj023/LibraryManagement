116fa6bd0c95165dc9329dfb417d058f87c8009c
Suraj023 <surajverma023@gmail.com>
2025-08-15 16:28:04 +0530

    Used Lambok Annotation to remove the constructers and settter and getters

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -1,8 +1,10 @@
 package com.library.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,23 +13,4 @@ public class Book {
     private String title;
     private String author;
     private String isbn;
-
-    // Constructors
-    public Book() {}
-
-    public Book(String title, String author, String isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
-
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-    public String getIsbn() { return isbn; }
-    public void setIsbn(String isbn) { this.isbn = isbn; }
 }
--- src/main/java/com/library/model/User.java
+++ src/main/java/com/library/model/User.java
@@ -1,9 +1,13 @@
 package com.library.model;
 
+import java.util.List;
+
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,30 +16,6 @@ public class User {
     private String name;
     private String email;
     private String password;
-
-    // Constructors
-    public User() {}
-
-    public User(String name, String email,String password) {
-        this.name = name;
-        this.email = email;
-        this.password=password;
-    }
-
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    private List<String> roles;
     
 }
