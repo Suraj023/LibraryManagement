d46113ff4cb4c42346e09030fa47ad2515b9961f
Suraj023 <surajverma023@gmail.com>
2025-08-16 00:09:54 +0530

    Change made to  disable the session establishment

--- src/main/java/com/library/config/SecurityConfig.java
+++ src/main/java/com/library/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -49,6 +50,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable()) // Disable CSRF for API
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Disable session
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/users/**").permitAll() // Protected
                 .requestMatchers("/api/books/**").permitAll() // Public
@@ -58,4 +61,5 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 }
