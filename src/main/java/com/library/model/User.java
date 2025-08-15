1b194d7c15accb784a1856c16cbdea49b7769b62
Suraj023 <surajverma023@gmail.com>
2025-08-15 16:22:07 +0530

    Spring Security Changes

--- src/main/java/com/library/model/User.java
+++ src/main/java/com/library/model/User.java
@@ -11,13 +11,15 @@ public class User {
 
     private String name;
     private String email;
+    private String password;
 
     // Constructors
     public User() {}
 
-    public User(String name, String email) {
+    public User(String name, String email,String password) {
         this.name = name;
         this.email = email;
+        this.password=password;
     }
 
     // Getters and Setters
@@ -27,4 +29,13 @@ public class User {
     public void setName(String name) { this.name = name; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
 }
